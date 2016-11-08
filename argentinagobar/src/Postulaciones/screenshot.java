331fc6d246ea9fb96448dfde9d5e5c31c3f814ee
aluna <aluna@aluna-Precision-Tower-3620>
2016-11-08 12:09:23 -0300

    actualizacion de casos

--- argentinagobar/src/Postulaciones/screenshot.java
+++ argentinagobar/src/Postulaciones/screenshot.java
@@ -13,8 +13,6 @@ import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.remote.LocalFileDetector;
 import org.openqa.selenium.support.ui.Select;
 import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 
 
 
@@ -39,41 +37,38 @@ public class screenshot {
   @Test
   public void testAltaSinAdjunto() throws Exception {
 	  /* maximizar navegador*/
-	driver.manage().window().maximize();
-    driver.get(baseUrl + "postulaciones");
-    driver.findElement(By.id("edit-nombres")).clear();
-    driver.findElement(By.id("edit-nombres")).sendKeys("ariana");
-    driver.findElement(By.id("edit-apellido")).clear();
-    driver.findElement(By.id("edit-apellido")).sendKeys("lopez");
-    driver.findElement(By.id("edit-telefono")).clear();
-    driver.findElement(By.id("edit-telefono")).sendKeys("43211212");
-    driver.findElement(By.id("edit-dni")).clear();
-    driver.findElement(By.id("edit-dni")).sendKeys("23111222");
-    driver.findElement(By.id("edit-mail")).clear();
-    driver.findElement(By.id("edit-mail")).sendKeys("asd@asd.com");
-    driver.findElement(By.xpath("//div[@id='edit-pais-residencia']/label")).click();
-    new Select(driver.findElement(By.id("edit-provincia"))).selectByVisibleText("Buenos Aires");
-    driver.findElement(By.id("edit-localidad")).clear();
-    driver.findElement(By.id("edit-localidad")).sendKeys("asdasd");
-    driver.findElement(By.xpath("//div[@id='edit-genero']/label")).click();
-    new Select(driver.findElement(By.id("edit-nivel-de-estudio"))).selectByVisibleText("Secundaria");
-    new Select(driver.findElement(By.id("edit-estado-cursada"))).selectByVisibleText("Completo");
-    driver.findElement(By.id("edit-titulo-obtenido")).clear();
-    driver.findElement(By.id("edit-titulo-obtenido")).sendKeys("asd");
-    new Select(driver.findElement(By.id("edit-aos-de-experiencia-laboral-en-apn"))).selectByVisibleText("0-1 Años");
-    driver.findElement(By.xpath("//div[@id='edit-trabajo-en-sector-publico']/label")).click();
-    driver.findElement(By.id("edit-en-donde")).clear();
-    driver.findElement(By.id("edit-en-donde")).sendKeys("asd");
-    driver.findElement(By.xpath("//div[@id='edit-trabaja-actualmente']/label")).click();
-    driver.findElement(By.id("edit-nombre-organizacion")).clear();
-    driver.findElement(By.id("edit-nombre-organizacion")).sendKeys("asd");
-    new Select(driver.findElement(By.id("edit-jerarquia"))).selectByVisibleText("Jefe/Superior");
-    driver.findElement(By.xpath("//div[@id='edit-lugar-de-trabajo-deseado']/label")).click();
-    driver.findElement(By.xpath("//div[@id='edit-desea-postularse']/label")).click();
-    driver.findElement(By.xpath("//div[@id='edit-discapacidad']/label")).click();
-    driver.findElement(By.xpath("//div[@id='edit-discapacidad-certificado']/label")).click();
-    driver.findElement(By.id("edit-comentario")).clear();
-    driver.findElement(By.id("edit-comentario")).sendKeys("asd");
+		driver.manage().window().maximize();
+		/* maximizar navegador*/
+	    driver.get(baseUrl + "postulaciones");
+	    driver.findElement(By.id("edit-nombres")).clear();
+	    driver.findElement(By.id("edit-nombres")).sendKeys("ariana");
+	    driver.findElement(By.id("edit-apellido")).clear();
+	    driver.findElement(By.id("edit-apellido")).sendKeys("lopez");
+	    driver.findElement(By.id("edit-telefono")).clear();
+	    driver.findElement(By.id("edit-telefono")).sendKeys("43211212");
+	    driver.findElement(By.id("edit-dni")).clear();
+	    driver.findElement(By.id("edit-dni")).sendKeys("23111222");
+	    driver.findElement(By.id("edit-mail")).clear();
+	    driver.findElement(By.id("edit-mail")).sendKeys("asd@asd.com");
+	    driver.findElement(By.xpath("//div[@id='edit-pais-residencia']/label")).click();
+	    new Select(driver.findElement(By.id("edit-provincia"))).selectByVisibleText("Capital Federal");
+	    driver.findElement(By.id("edit-localidad")).clear();
+	    driver.findElement(By.id("edit-localidad")).sendKeys("san telmo");
+	    new Select(driver.findElement(By.id("edit-fecha-nacimiento-day"))).selectByVisibleText("5");
+	    new Select(driver.findElement(By.id("edit-fecha-nacimiento-year"))).selectByVisibleText("1980");
+	    driver.findElement(By.xpath("//div[@id='edit-genero']/div/label")).click();
+	    new Select(driver.findElement(By.id("edit-nivel-de-estudio"))).selectByVisibleText("Secundaria Completo");
+	    driver.findElement(By.id("edit-titulo-obtenido")).clear();
+	    driver.findElement(By.id("edit-titulo-obtenido")).sendKeys("titulo completo");
+	    new Select(driver.findElement(By.id("edit-aos-de-experiencia-laboral-en-apn"))).selectByVisibleText("0 - 3 Años");
+	    driver.findElement(By.xpath("//div[@id='edit-trabajo-en-sector-publico']/label")).click();
+	    driver.findElement(By.xpath("//div[@id='edit-trabajo-en-sector-publico']/label[2]")).click();
+	    driver.findElement(By.xpath("//div[@id='edit-trabaja-actualmente']/label")).click();
+	    driver.findElement(By.xpath("//div[@id='edit-trabaja-actualmente']/label[2]")).click();
+	    new Select(driver.findElement(By.id("edit-provincia-donde-trabajar"))).selectByVisibleText("Capital Federal");
+	    driver.findElement(By.xpath("//div[@id='edit-discapacidad']/label[2]")).click();
+	    driver.findElement(By.id("edit-comentario")).clear();
+	    driver.findElement(By.id("edit-comentario")).sendKeys("asd");
     LocalFileDetector detector = new LocalFileDetector();
     File file = detector.getLocalFile("/home/aluna/Desktop/archivos para adjuntar/pdf.pdf");
     driver.findElement(By.name("files[cv]")).sendKeys(file.getAbsolutePath());
